0b460362790ab2a7a44859cf78cada52397e2e78
Joseph <josephopio11@gmail.com>
2024-01-23 10:59:27 +0800

    Text editor using froala

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,183 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+//Froala Editor
+import FroalaEditor from "react-froala-wysiwyg";
+import "froala-editor/css/froala_editor.pkgd.min.css";
+
+// Alignment
+import "froala-editor/js/plugins/align.min.js";
+
+// Quote
+import "froala-editor/js/plugins/quote.min.js";
+
+// Paragraph-style
+import "froala-editor/js/plugins/paragraph_style.min.js";
+
+// Paragraph format
+import "froala-editor/js/plugins/paragraph_format.min.js";
+
+// Save
+import "froala-editor/js/plugins/save.min.js";
+
+// Quick insert
+import "froala-editor/js/plugins/quick_insert.min.js";
+import "froala-editor/css/plugins/quick_insert.min.css";
+
+// Table
+import "froala-editor/js/plugins/table.min.js";
+import "froala-editor/css/plugins/table.min.css";
+
+// Track changes
+import "froala-editor/js/plugins/track_changes.min.js";
+
+// Spell Checker
+import "froala-editor/js/third_party/spell_checker.min.js";
+import "froala-editor/css/third_party/spell_checker.min.css";
+
+// Video Plugin
+import "froala-editor/js/plugins/video.min.js";
+import "froala-editor/css/plugins/video.min.css";
+
+// Url Plugin
+import "froala-editor/js/plugins/url.min.js";
+
+// Link
+import "froala-editor/js/plugins/link.min.js";
+
+// Lists
+import "froala-editor/js/plugins/lists.min.js";
+
+// Inline style
+import "froala-editor/js/plugins/inline_style.min.js";
+
+// Inline class
+import "froala-editor/js/plugins/inline_class.min.js";
+
+// Line breaker
+import "froala-editor/js/plugins/line_breaker.min.js";
+import "froala-editor/css/plugins/line_breaker.min.css";
+
+// Line Height
+import "froala-editor/js/plugins/line_height.min.js";
+
+// Character counter
+import "froala-editor/js/plugins/char_counter.min.js";
+import "froala-editor/css/plugins/char_counter.min.css";
+
+// Code beautifier
+import "froala-editor/js/plugins/code_beautifier.min.js";
+
+// Code viewer
+import "froala-editor/js/plugins/code_view.min.js";
+import "froala-editor/css/plugins/code_view.min.css";
+
+// Emoticons
+import "froala-editor/js/plugins/emoticons.min.js";
+import "froala-editor/css/plugins/emoticons.min.css";
+
+// Embedly
+import "froala-editor/js/third_party/embedly.min.js";
+import "froala-editor/css/third_party/embedly.min.css";
+
+// Draggable
+import "froala-editor/js/plugins/draggable.min.js";
+import "froala-editor/css/plugins/draggable.min.css";
+
+// Colors
+import "froala-editor/js/plugins/colors.min.js";
+import "froala-editor/css/plugins/colors.min.css";
+
+// Entities
+import "froala-editor/js/plugins/entities.min.js";
+
+//filesManager
+// TODO: This needs to be studied
+import "froala-editor/js/plugins/files_manager.min.js";
+
+// file
+import "froala-editor/js/plugins/file.min.js";
+import "froala-editor/css/plugins/file.min.css";
+
+//fontAwesome
+import "froala-editor/js/third_party/font_awesome.min.js";
+import "froala-editor/css/third_party/font_awesome.min.css";
+
+// Help plugin
+import "froala-editor/js/plugins/help.min.js";
+import "froala-editor/css/plugins/help.min.css";
+
+//fullscreen
+import "froala-editor/js/plugins/fullscreen.min.js";
+import "froala-editor/css/plugins/fullscreen.min.css";
+
+// font size
+import "froala-editor/js/plugins/font_size.min.js";
+
+// font family
+import "froala-editor/js/plugins/font_family.min.js";
+
+// image
+import "froala-editor/js/plugins/image.min.js";
+import "froala-editor/css/plugins/image.min.css";
+
+// image manager
+import "froala-editor/js/plugins/image_manager.min.js";
+import "froala-editor/css/plugins/image_manager.min.css";
+
+// image tui
+import "froala-editor/js/third_party/image_tui.min.js";
+import "froala-editor/css/third_party/image_tui.min.css";
+
+import "froala-editor/js/third_party/showdown.min.js";
+import { toast } from "sonner";
+
+const Editor = () => {
+  const [zenMode, setZenMode] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [model, setModel] = useState(() => {
+    return localStorage.getItem("savedHtml") || "";
+  });
+  console.log(zenMode);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="px-4">
+      <h1 className="text-3xl my-10">Text Editor</h1>
+      <Button className="my-3" onClick={() => setZenMode(!zenMode)}>
+        Editor
+      </Button>
+      <div className="prose prose-lg pb-7">
+        <FroalaEditor
+          model={model}
+          onModelChange={(e: string) => setModel(e)}
+          config={{
+            placeholderText: "Please type your content here",
+            saveInterval: 2000,
+            charCounterCount: true,
+            charCounterMax: 50,
+            events: {
+              "charCounter.exceeded": function () {
+                toast.error("You have exceeded the maximum character limit");
+              },
+              "save.before": function (html: string) {
+                localStorage.setItem("savedHtml", html);
+                toast.info("Saving...", {
+                  duration: 1000,
+                  position: "bottom-center",
+                });
+              },
+              "save.after": function () {
+                toast.success("Saved!");
+              },
+            },
+          }}
+          tag="textarea"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Editor;
